f5154f2e3ea02fe9c90acb47a1e2e40a582cb2cb
Eitan Chatav <eitan@EEM109LMBP-1.local>
2020-03-24 10:43:04 -0700

    IndexedMonadTransFree

--- src/Control/Monad/Trans/Indexed/Free.hs
+++ src/Control/Monad/Trans/Indexed/Free.hs
@@ -1,28 +1,47 @@
 {-# LANGUAGE
-    DeriveFunctor
+    DataKinds
+  , DeriveFunctor
   , FlexibleInstances
+  , FunctionalDependencies
   , GADTs
   , LambdaCase
+  , MultiParamTypeClasses
+  , PolyKinds
   , QuantifiedConstraints
   , RankNTypes
+  , StandaloneDeriving
+  , UndecidableInstances
 #-}
 
 module Control.Monad.Trans.Indexed.Free
-  ( FreeIx (..)
+  ( IndexedMonadTransFree (..)
+  , liftIxF
+  , FreeIx (..)
   , FreeIxF (..)
   ) where
 
 import Control.Monad.Trans
 import Control.Monad.Trans.Indexed
+import Data.Function ((&))
+
+class IndexedMonadTrans t => IndexedMonadTransFree f t | t -> f where
+  wrapIx :: Monad m => f i j (t j k m x) -> t i k m x
+
+liftIxF
+  :: ( forall i j. Functor (f i j)
+     , IndexedMonadTransFree f t
+     , Monad m
+     ) => f i j x -> t i j m x
+liftIxF = wrapIx . fmap return
 
 data FreeIxF f i j m x where
   PureIx :: x -> FreeIxF f i i m x
-  JoinIx :: f i j (FreeIx f j k m x) -> FreeIxF f i k m x
+  WrapIx :: f i j (FreeIx f j k m x) -> FreeIxF f i k m x
 instance (forall i j. Functor (f i j), Monad m)
   => Functor (FreeIxF f i j m) where
     fmap f = \case
       PureIx x -> PureIx $ f x
-      JoinIx fm -> JoinIx $ fmap (fmap f) fm
+      WrapIx fm -> WrapIx $ fmap (fmap f) fm
 
 newtype FreeIx f i j m x = FreeIx {runFreeIx :: m (FreeIxF f i j m x)}
 instance (forall i j. Functor (f i j), Monad m)
@@ -43,4 +62,8 @@ instance (forall i j. Functor (f i j))
   => IndexedMonadTrans (FreeIx f) where
     ixJoin (FreeIx mm) = FreeIx $ mm >>= \case
       PureIx (FreeIx m) -> m
-      JoinIx fm -> return $ JoinIx $ fmap ixJoin fm
+      WrapIx fm -> return $ WrapIx $ fmap ixJoin fm
+instance
+  ( forall i j. Functor (f i j)
+  ) => IndexedMonadTransFree f (FreeIx f) where
+  wrapIx = FreeIx . return . WrapIx
