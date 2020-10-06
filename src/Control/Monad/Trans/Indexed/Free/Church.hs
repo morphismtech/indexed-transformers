8381835472dc105a466370bb3f93107df2492a75
Eitan Chatav <eitan.chatav@gmail.com>
2020-10-05 17:52:05 -0700

    Update Church.hs

--- src/Control/Monad/Trans/Indexed/Free/Church.hs
+++ src/Control/Monad/Trans/Indexed/Free/Church.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE
-    PolyKinds
+    GADTs
+  , PolyKinds
   , QuantifiedConstraints
   , RankNTypes
 #-}
@@ -9,11 +10,26 @@ module Control.Monad.Trans.Indexed.Free.Church
   ( FreeIx (..)
   ) where
 
+import Control.Monad.Trans
 import Control.Monad.Trans.Indexed
 import Control.Monad.Trans.Indexed.Free
 
 newtype FreeIx g i j m x = FreeIx
-  {getFreeIx :: forall t m. (IndexedMonadTrans t, Monad m)
+  {getFreeIx :: forall t. (IndexedMonadTrans t, Monad m)
     => (forall i j x. g i j x -> t i j m x) -> t i j m x}
 instance (Silo g, Monad m) => Functor (FreeIx g i j m) where
   fmap f (FreeIx k) = FreeIx $ \step -> fmap f (k step)
+instance (Silo f, i ~ j, Monad m)
+  => Applicative (FreeIx f i j m) where
+    pure x = FreeIx $ const $ pure x
+    (<*>) = undefined
+instance (Silo f, i ~ j, Monad m)
+  => Monad (FreeIx f i j m) where
+    return x = FreeIx $ const $ return x
+    (>>=) = undefined
+instance (Silo f, i ~ j)
+  => MonadTrans (FreeIx f i j) where
+    lift m = FreeIx $ const $ lift m
+-- instance Silo f
+--   => IndexedMonadTrans (FreeIx f) where
+--     ixJoin (FreeIx k) = FreeIx $ \j -> j _
