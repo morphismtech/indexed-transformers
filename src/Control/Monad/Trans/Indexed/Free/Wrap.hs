8cb8535f2845c5ddfdb4557a7cac6d50a7d552d3
Eitan Chatav <eitan.chatav@gmail.com>
2020-10-05 17:51:45 -0700

    Update Wrap.hs

--- src/Control/Monad/Trans/Indexed/Free/Wrap.hs
+++ src/Control/Monad/Trans/Indexed/Free/Wrap.hs
@@ -20,23 +20,23 @@ import Control.Monad.Trans.Indexed
 import Control.Monad.Trans.Indexed.Free
 
 data WrapIx f i j m x where
-  Unwrapped :: x -> WrapIx f i i m x
-  Wrapped :: f i j (FreeIx f j k m x) -> WrapIx f i k m x
+  Unwrap :: x -> WrapIx f i i m x
+  Wrap :: f i j (FreeIx f j k m x) -> WrapIx f i k m x
 instance (Silo f, Monad m)
   => Functor (WrapIx f i j m) where
     fmap f = \case
-      Unwrapped x -> Unwrapped $ f x
-      Wrapped fm -> Wrapped $ fmap (fmap f) fm
+      Unwrap x -> Unwrap $ f x
+      Wrap fm -> Wrap $ fmap (fmap f) fm
 instance SFunctor WrapIx where
   smap f = \case
-    Unwrapped x -> Unwrapped x
-    Wrapped y -> Wrapped (f (fmap (smap f) y))
+    Unwrap x -> Unwrap x
+    Wrap y -> Wrap (f (fmap (smap f) y))
 instance SFoldable WrapIx where
   sfoldMap f = \case
-    Unwrapped x -> return x
-    Wrapped y -> ixBind (sfoldMap f) (f y)
+    Unwrap x -> return x
+    Wrap y -> ixBind (sfoldMap f) (f y)
 instance SPointed WrapIx where
-  slift = Wrapped . fmap return
+  slift = Wrap . fmap return
 
 newtype FreeIx f i j m x = FreeIx {runFreeIx :: m (WrapIx f i j m x)}
 instance SFunctor FreeIx where
@@ -45,28 +45,31 @@ instance SFoldable FreeIx where
   sfoldMap f (FreeIx m) = ixBind (sfoldMap f) (lift m)
 instance SPointed FreeIx where
   slift = FreeIx . return . slift
+instance SMonad FreeIx where
+  sbind = sfoldMap
 instance (Silo f, Monad m)
   => Functor (FreeIx f i j m) where
     fmap f (FreeIx m) = FreeIx $ fmap (fmap f) m
 instance (Silo f, i ~ j, Monad m)
   => Applicative (FreeIx f i j m) where
-    pure = FreeIx . pure . Unwrapped
+    pure = FreeIx . pure . Unwrap
     (<*>) = ixAp
 instance (Silo f, i ~ j, Monad m)
   => Monad (FreeIx f i j m) where
-    return = FreeIx . return . Unwrapped
+    return = FreeIx . return . Unwrap
     (>>=) = flip ixBind
 instance (Silo f, i ~ j)
   => MonadTrans (FreeIx f i j) where
-    lift = FreeIx . fmap Unwrapped
+    lift = FreeIx . fmap Unwrap
 instance Silo f
   => IndexedMonadTrans (FreeIx f) where
     ixJoin (FreeIx mm) = FreeIx $ mm >>= \case
-      Unwrapped (FreeIx m) -> m
-      Wrapped fm -> return $ Wrapped $ fmap ixJoin fm
+      Unwrap (FreeIx m) -> m
+      Wrap fm -> return $ Wrap $ fmap ixJoin fm
 instance
   ( Silo f
   , Monad m
   , i ~ j
   ) => MonadFree (f i j) (FreeIx f i j m) where
-    wrap = FreeIx . return . Wrapped
+    wrap = FreeIx . return . Wrap
+instance IxFree FreeIx
