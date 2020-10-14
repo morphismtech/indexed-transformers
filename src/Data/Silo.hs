dbd7c78568c8bc8e30c658cf611316a470be9cc5
Eitan Chatav <eitan.chatav@gmail.com>
2020-10-14 08:40:59 -0700

    Update Silo.hs

--- src/Data/Silo.hs
+++ src/Data/Silo.hs
@@ -20,10 +20,11 @@ import Data.Kind
 {- |
 A higher quiver is is a higher kinded type,
 
-@p :: k -> k -> Type -> Type@
+@g :: k -> k -> Type -> Type@
 
-  * where vertices are types @x :: k@,
-  * and arrows from @x@ to @y@ are type constructors @p x y :: Type -> Type@.
+  * where vertices are types @i :: k@,
+  * and arrows from source @i@ to target @j@
+    labeled by datatype @x@ are terms @g i j x@.
 
 `Silo` is a quantified constraint on a higher quiver,
 capturing that it is a `Functor` for any source and target.
@@ -39,8 +40,13 @@ data DVDCommand
   -> Bool -- ^ is drive full finally?
   -> Type -- ^ return type
   -> Type where
-  Insert :: x -> DVD -> DVDCommand 'False 'True x
-  Eject :: (DVD -> x) -> DVDCommand 'True 'False x
+  Insert
+    :: DVD -- ^ a DVD to insert
+    -> x -- ^ then what to do
+    -> DVDCommand 'False 'True x -- ^ empty to full
+  Eject
+    :: (DVD -> x) -- ^ what to do with an ejected DVD
+    -> DVDCommand 'True 'False x -- ^ full to empty
 :}
 >>> deriving instance Functor (DVDCommand i j)
 >>> () :: Silo DVDCommand => ()
@@ -48,7 +54,8 @@ data DVDCommand
 -}
 type Silo (g :: k -> k -> Type -> Type) = forall i j. Functor (g i j)
 
-{- | `Lance` is a left Kan extension, it is the "free silo on a higher quiver".
+{- | `Lance` is a left Kan extension,
+it is the free `Silo` on a higher quiver.
 
 >>> type DVD = String
 >>> :{
@@ -61,13 +68,23 @@ data DVDCommand
   Eject :: DVDCommand 'True 'False DVD
 :}
 
-@DVDCommand@ expresses exactly the signature we want to describe our commands. 
+@DVDCommand@ expresses exactly the signature we want to describe our commands.
 Unfortunately, @DVDCommand@ cannot be a `Silo`,
-it's just not polymorphic enough. What would do with `fmap (+1)`?
-@Lance DVDCommand@ however _is_ a `Silo`.
+it's just not polymorphic enough.
+@Lance DVDCommand@ however _is_ a `Silo`,
+isomorphic to the previous definition of @DVDCommand@.
 
-`Lance` is the "free functor", meaning it's left adjoint to "forgetting"
-the `Silo` constraint.
+>>> () :: Silo (Lance DVDCommand) => ()
+()
+>>> let insert dvd x = liftLance (Insert dvd) $> x
+>>> :type insert
+insert :: DVD -> x -> Lance DVDCommand 'False 'True x
+>>> let eject k = k <$> liftLance Eject
+>>> :type eject
+eject :: (DVD -> x) -> Data.Silo.Lance DVDCommand 'True 'False x
+
+`Lance` is a free functor,
+meaning it's left adjoint to forgetting the `Silo` constraint.
 -}
 data Lance f i j x where
   Lance :: (x -> y) -> f i j x -> Lance f i j y
@@ -77,5 +94,5 @@ mapLance :: (forall x. f i j x -> g i j x) -> Lance f i j x -> Lance g i j x
 mapLance g (Lance f x) = Lance f (g x)
 liftLance :: g i j x -> Lance g i j x
 liftLance = Lance id
-lowerLance :: Silo g => Lance g i j x -> g i j x
+lowerLance :: Functor (g i j) => Lance g i j x -> g i j x
 lowerLance (Lance f x) = fmap f x
