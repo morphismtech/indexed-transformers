02c173fc5a6f21c195c32726de93de0d75e3a910
Eitan Chatav <eitan.chatav@gmail.com>
2025-11-17 13:05:56 -0800

    Ixer

--- src/Control/Monad/Trans/Indexed/State/Kan.hs
+++ src/Control/Monad/Trans/Indexed/State/Kan.hs
@@ -3,29 +3,25 @@ module Control.Monad.Trans.Indexed.State.Kan
   , ReadStx (..)
   ) where
 
+import Control.Monad
 import Control.Monad.Reader
 import Control.Monad.State
 import Control.Monad.Trans.Indexed.Codensity
 import Control.Monad.Trans.Indexed.Free
 import Control.Monad.Trans.Indexed.Free.Wrap
 
-type StateIx = CodensityIx (FreeIx ReadStx)
+type StateIx = CodensityIx (FreeIx (Ixer ReadStx))
 
 data ReadStx s t x where
-  NoOpStx :: ReadStx s s ()
   AskStx :: ReadStx s s s
-  LocalStx
-    :: (u -> s) -> (x -> y)
-    -> ReadStx s t x -> ReadStx u t y
-instance Functor (ReadStx s t) where
-  fmap = LocalStx id
 
-instance (s ~ t, Monad m, IxMonadTransFree freeIx)
-  => MonadReader s (freeIx ReadStx s t m) where
-    ask = liftFreeIx AskStx
-    local f m = lowerCodensityIx $ do
+instance (s ~ t, Monad m)
+  => MonadReader s (FreeIx (Ixer ReadStx) s t m) where
+    ask = liftFreerIx AskStx
+    local f m = do
       s <- ask
-      put (f s)
-      x <- liftCodensityIx m
-      put s
-      return x
+      lowerCodensityIx $ do
+        put (f s)
+        x <- liftCodensityIx m
+        put s
+        return x
